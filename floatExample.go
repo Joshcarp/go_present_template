dc5c167c4a3b635a4d86fc58e4735c32c139f184
carpeggj <joshuacarpeggiani@gmail.com>
2019-06-20 09:21:26 +1000

    Fix floatexample.go

--- floatExample.go
+++ floatExample.go
@@ -10,15 +10,17 @@ import (
 func main() {
 	var a = 0.1
 	var b = 0.3
+
+	fmt.Println("Float datatype:")
 	fmt.Printf("%v + %v + %v == %v : %v\n", a, a, a, b, (a+a+a == b))
-
-	// Printing out with 20 sig figs we can see where the problem is.
-	fmt.Printf("%.20f + %.20f + %.20f == %.20f : %v\n", a, a, a, b, (a+a+a == b))
-
-	// Using a decimal datatype can mitigate these errors as decimal fractions can be represented exactly
+	// // Printing out with 20 sig figs we can see where the problem is.
+	fmt.Printf("%.20f + %.20f + %.20f == %.20f : %v\n\n", a, a, a, b, (a+a+a == b))
+	//
+	// // Using a decimal datatype can mitigate these errors as decimal fractions can be represented exactly
+	fmt.Println("Decimal datatype:")
 	c := decimal.MustParseDecimal64("0.1")
 	d := decimal.MustParseDecimal64("0.3")
-
-	fmt.Printf("%.20f + %.20f + %.20f == %.20f : %v\n", c, c, c, d, (c.Add(c).Add(c) == d))
+	//
+	fmt.Printf("%.20f + %.20f + %.20f == %.20f : %v\n\n", c, c, c, d, (c.Add(c).Add(c) == d))
 
 }
