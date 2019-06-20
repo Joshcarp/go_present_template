1eba0c2e0b769e32b8613e201d4e5b1a53562ad1
carpeggj <joshuacarpeggiani@gmail.com>
2019-06-20 11:29:12 +1000

    Slide stuff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # decimal-slides
 Slides and examples for gophercon
+- install go present with https://github.com/golang/talks
+- info about markup language used in https://godoc.org/golang.org/x/tools/present
--- examples/floatExample.go
+++ examples/floatExample.go
@@ -10,13 +10,12 @@ import (
 func main() {
 	var a = 0.1
 	var b = 0.3
-
 	fmt.Println("Float datatype:")
 	fmt.Printf("%v + %v + %v == %v : %v\n", a, a, a, b, (a+a+a == b))
 	// // Printing out with 20 sig figs we can see where the problem is.
 	fmt.Printf("%.20f + %.20f + %.20f == %.20f : %v\n\n", a, a, a, b, (a+a+a == b))
 	//
-	// // Using a decimal datatype can mitigate these errors as decimal fractions can be represented exactly
+	// Using a decimal datatype can mitigate these errors as decimal fractions can be represented exactly
 	fmt.Println("Decimal datatype:")
 	c := decimal.MustParseDecimal64("0.1")
 	d := decimal.MustParseDecimal64("0.3")
