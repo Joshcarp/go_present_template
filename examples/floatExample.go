aa37587a006404a2d9384cd5edb90f2fd326800b
carpeggj <joshuacarpeggiani@gmail.com>
2019-06-26 09:19:01 +1000

    Floating point example

--- examples/floatExample.go
+++ examples/floatExample.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/anz-bank/decimal"
 )
 
 func main() {
-	var a decimal.Decimal64
-	b := decimal.MustParseDecimal64("0.1")
-	c := decimal.MustParseDecimal64("0.3")
-	d := decimal.NewDecimal64FromInt64(123456)
+	a := 0.1
+	b := 0.3
 
-	fmt.Println(a, b, c, d)
+	fmt.Printf("%f, %f, %f, %f, 3*a == b; %v\n", a, a, a, b, 3*a == b)
+
+	fmt.Printf("%.20f, %.20f, %.20f, %.20f, 3*a == b; %v\n", a, a, a, b, 3*a == b)
 
 }
