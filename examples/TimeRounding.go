e5c186e7c7cc1f5047f796690fd03c0b7e7dee93
carpeggj <joshuacarpeggiani@gmail.com>
2019-06-27 21:13:55 +1000

    Upadate

--- examples/TimeRounding.go
+++ examples/TimeRounding.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/anz-bank/decimal"
 )
 
 func main() {
+	t := 60 * 60 * 100
 	// Floating point var for keeping track of time
 	var time float32
 	// Decimal floating point var for keeping track of time
-	var decTime decimal.Decimal64
-	// Given time over 100 hours
-	for i := 0; i < 100*60*60*10; i++ {
+
+	for seconds := 0; seconds < t; seconds++ {
 		// Adding 0.1 repeatedly compounds rounding error
-		time += 0.1
-		decTime = decTime.Add(decimal.MustParseDecimal64("0.1"))
+		for tenths := 0; tenths < 10; tenths++ {
+			time += 0.1
+		}
 	}
-	diff := decTime.Sub(decimal.MustParseDecimal64(fmt.Sprintf("%f", time)))
-	fmt.Printf(`Seconds taken in floating point: %f
-Real time taken in Decimal     : %f
-Time difference                : %f`, time, decTime, diff)
+	diff := float32(t) - time
+	fmt.Printf(`Seconds taken in floating point: %f s
+Real time taken                : %d s
+Time difference                :  %f s`, time, t, diff)
 
 }
