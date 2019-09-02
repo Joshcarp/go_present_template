581b6b5efd6f1fed67c68fcf17ba3abd8297db52
Joshua <joshuacarpeggiani@gmail.com>
2019-09-02 15:47:02 +1000

    Add example of unpacking

---
+++ examples/ansFunction.go
@@ -0,0 +1,12 @@
+package main
+
+// START OMIT
+func (d Decimal64) Add(e Decimal64) Decimal64 {
+	dp := d.getParts()
+	ep := e.getParts()
+	var ans DecParts
+	// Arithmetic here ...
+	return newFromParts(ans)
+}
+
+// End OMIT
