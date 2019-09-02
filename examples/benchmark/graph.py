14b34f35f4efaf401441eddeeb793bb422e9ce06
Joshua <joshuacarpeggiani@gmail.com>
2019-09-02 20:40:03 +1000

    graph to graph first graphs again

--- examples/benchmark/graph.py
+++ examples/benchmark/graph.py
@@ -17,7 +17,7 @@ def convertToCsv(filename):
     regex = r"(?:BenchmarkDecimal\/dd)(?P<TestName>\w*)(?:.decTest_)(?P<LibraryName>\w*)(?:(#\d*)*-\d\s*)(?P<iterations>\d*)(?:\s*)(?P<time>\d*)"
     matches = re.finditer(regex, line)
     for mat in matches:
-        c.writerow([mat.group("TestName"), mat.group("LibraryName"), mat.group("iterations"), mat.group("time")])
+        c.writerow([mat.group("TestName"), mat.group("LibraryName"), mat.group("iterations"), int(mat.group("time"))/1000000])
     output.close()
     return
 
@@ -27,7 +27,7 @@ def convertToCsv(filename):
 
 def generate_graph(responsetime_filename):
     data = pandas.read_csv(responsetime_filename)
-
+    plt.rcParams.update({'font.size': 17})
     operations = data.TestName.unique()
     for operation in operations:
         element = data.loc[data["TestName"] == operation]
@@ -41,9 +41,10 @@ def generate_graph(responsetime_filename):
         plt.figure(figsize=(10,6))
         x = np.arange(len(element.index))
         dupeInd = np.arange(len(dupes.index))
-        bar_lst2 = plt.bar(dupeInd, dupes.time.tolist())
+        bar_lst2 = []
+        # bar_lst2 = plt.bar(dupeInd, dupes.time.tolist())
         bar_lst = plt.bar(x, element.time.tolist())
-        
+       
         colors = cycle([(232/255, 62/255, 93/255, 0.3),(65/255, 186/255, 99/255, 0.3),(87/255, 126/255, 199/255, 0.3),(232/255, 165/255, 0, 0.3)])
         for i in range(len(bar_lst)):
             bar_lst[i].set_color(next(colors))
@@ -52,11 +53,12 @@ def generate_graph(responsetime_filename):
             bar_lst2[i].set_color(next(colors))
         plt.xticks(x, element.LibraryName)
         plt.xticks(rotation=10)
-        plt.ylabel('ns/operation')
+
+        plt.ylabel('ms/operation')
         plt.title(operation + " Benchmark")
-        plt.savefig(os.path.abspath('../../content/')+f"/img/{operation}_optimised_1.png", dpi=300, figsize=(50,25))
+        plt.savefig(os.path.abspath('../../content/')+f"/img/{operation}.png", dpi=300, figsize=(50,25),  bbox_inches = 'tight')
 
 if __name__ == "__main__":
     # os.system("go test -bench=. > results.txt")
-    convertToCsv("results.txt")
+    convertToCsv("resNoPoint.txt")
     generate_graph("results.csv")
